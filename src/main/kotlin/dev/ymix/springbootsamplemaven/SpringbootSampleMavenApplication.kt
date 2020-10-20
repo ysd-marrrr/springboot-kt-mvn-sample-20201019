49a79f4246a5011be44653b08de92952afcff28b
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-10-20 21:54:04 +0900

    アプリケーションが起動する状態でSpringBootのプロジェクトを立ち上げた

---
+++ src/main/kotlin/dev/ymix/springbootsamplemaven/SpringbootSampleMavenApplication.kt
@@ -0,0 +1,11 @@
+package dev.ymix.springbootsamplemaven
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootSampleMavenApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootSampleMavenApplication>(*args)
+}
---
+++ src/test/kotlin/dev/ymix/springbootsamplemaven/SpringbootSampleMavenApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.ymix.springbootsamplemaven
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootSampleMavenApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
